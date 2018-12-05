b600582de8abc0d13800d66ed70789405d3692e7
Frans Spijkerman <fj.spijkerman@avans.nl>
2018-12-05 08:54:24 +0100

    V2.0

--- create-tables.php
+++ create-tables.php
@@ -6,4 +6,4 @@ require 'db.php';
 $sql = file_get_contents('create-tables.sql');
 $sql = str_replace("\xEF\xBB\xBF", '', $sql); // remove BOM
 $pdo->query($sql);
-header('location: .');
+header('location: customer-list.php');
